5d6ae05864360f4b901b2eadb15275722722a78b
Qiqi Zhao <qiqzhao@thoughtworks.com>
2018-01-15 21:19:27 +0800

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ function test {
 function build {
     docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean bootRepackage
     if [[ -z $DOCKER_REGISRTY ]]; then
-        DOCKER_REGISRTY=10.11.179.48:5000
+        DOCKER_REGISRTY=127.0.0.1:5000
     fi
     IMAGE_NAME=${DOCKER_REGISRTY}/tw-godless/order-service:${GO_PIPELINE_COUNTER}
     docker build -t $IMAGE_NAME .
@@ -22,7 +22,7 @@ function build {
 
 function deploy {
     if [[ -z $DOCKER_REGISRTY ]]; then
-      DOCKER_REGISRTY=127.0.0.1:5000
+      DOCKER_REGISRTY=10.11.179.48:5000
     fi
    
         IMAGE_NAME=${DOCKER_REGISRTY}/tw-godless/order-service:${GO_PIPELINE_COUNTER}
